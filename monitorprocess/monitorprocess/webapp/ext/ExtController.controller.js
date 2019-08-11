3e4705e554ffe1f951f155fda9e2cc745ac1ba2a
Vaibhav <vaibhav.agarwal02@sap.com>
2019-08-11 19:48:43 +0530

    Update latest code

---
+++ monitorprocess/monitorprocess/webapp/ext/ExtController.controller.js
@@ -0,0 +1,65 @@
+(function() {
+  "use strict";
+
+  sap.ui.controller("com.sap.cd.maco.monitor.ui.app.monitorprocesses.ext.ExtController", {
+    /**
+     * Determines which function should be called for the custom parameter
+     */
+    onCustomParams: function(sCustomParam) {
+      if (sCustomParam === "CardToDo") {
+        return this.getParamCardToDo;
+      } else if (sCustomParam === "CardStatus") {
+        return this.getParamCardStatus;
+      } else if (sCustomParam === "CardLoad") {
+        return this.getParamCardLoad;
+      } else {
+        throw new Error("unknown custom param " + sCustomParam);
+      }
+    },
+
+    getParamCardToDo: function(oNavigateParams) {
+      return {
+        selectionVariant: [
+          {
+            path: "Card",
+            operator: "EQ",
+            value1: "ToDo",
+            value2: null,
+            sign: "I"
+          }
+        ],
+        ignoreEmptyString: true
+      };
+    },
+
+    getParamCardStatus: function(oNavigateParams) {
+      return {
+        selectionVariant: [
+          {
+            path: "Card",
+            operator: "EQ",
+            value1: "Status",
+            value2: null,
+            sign: "I"
+          }
+        ],
+        ignoreEmptyString: true
+      };
+    },
+
+    getParamCardLoad: function(oNavigateParams) {
+      return {
+        selectionVariant: [
+          {
+            path: "Card",
+            operator: "EQ",
+            value1: "Load",
+            value2: null,
+            sign: "I"
+          }
+        ],
+        ignoreEmptyString: true
+      };
+    }
+  });
+})();
