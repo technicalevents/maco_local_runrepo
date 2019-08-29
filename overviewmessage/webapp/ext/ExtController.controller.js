368eeeb725b44f865e2ec65ff65107e2f6642a18
Vaibhav <vaibhav.agarwal02@sap.com>
2019-08-29 12:34:26 +0530

    Update

--- overviewmessage/webapp/ext/ExtController.controller.js
+++ overviewmessage/webapp/ext/ExtController.controller.js
@@ -1,35 +1,20 @@
-sap.ui.define([
-    "sap/ui/model/json/JSONModel",
-    "com/sap/cd/maco/mmt/ui/reuse/nav/Navigation"
-  ],
-  function(JSONModel, Navigation) {
-    'use strict';
+sap.ui.define([],
+function() {
+  'use strict';
 
-    sap.ui.controller(
-      'com.sap.cd.maco.monitor.ui.app.overviewmessages.ext.ExtController',
-      {
+  sap.ui.controller(
+    'com.sap.cd.maco.monitor.ui.app.overviewmessages.ext.ExtController',
+    {
 
-        /******************************************************************* */
-		/* LIFECYCLE METHODS */
-		/******************************************************************* */
-
-		/**
-		 * LifeCycle method Called when MessageListReport controller is instantiated.
-		 * @public
-		 */
-        onInit: function() {
-        	// Navigation Object
-            this.oNav = new Navigation(this.getOwnerComponent());
-        },
-        
-		/**
-		 * LifeCycle Method
-		 */
-		onAfterRendering: function(oEvent) {
-			this.getView().byId("mainView--ovpMain").addStyleClass("comSapCdMacoMmtUiMonitorMsgGraphTitle");
-		}
-      }
-    );
-  }
-);
-
+  /******************************************************************* */
+  /* LIFECYCLE METHODS */
+  /******************************************************************* */
+      
+    /**
+     * LifeCycle Method
+     */
+    onAfterRendering: function(oEvent) {
+      this.getView().byId("mainView--ovpMain").addStyleClass("comSapCdMacoMmtUiMonitorMsgGraphTitle");
+    }
+  });
+});
