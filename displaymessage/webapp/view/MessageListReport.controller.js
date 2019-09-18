14e2b66ee2bcbedc13f2debe2ea0c23cd0590574
Sukhram Bhamboo <sukhram.bhamboo@sap.com>
2019-09-18 06:18:25 +0000

    Download feature in Message List Page

--- displaymessage/webapp/view/MessageListReport.controller.js
+++ displaymessage/webapp/view/MessageListReport.controller.js
@@ -6,9 +6,12 @@ sap.ui.define(
     "com/sap/cd/maco/mmt/ui/reuse/monitor/Constants",
     "sap/ui/model/Sorter",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/thirdparty/hasher"
+    "sap/ui/thirdparty/hasher",
+    "sap/ui/thirdparty/jszip",
+	"sap/ui/core/util/File",
+	"sap/m/MessageBox"
   ],
-  function(ActionSmartTableController, SmartTableBindingUpdate, messageFormatter, Constants, Sorter, JSONModel, Hasher) {
+  function(ActionSmartTableController, SmartTableBindingUpdate, messageFormatter, Constants, Sorter, JSONModel, Hasher, JSZip, File, MessageBox) {
     'use strict';
 
     return ActionSmartTableController.extend(
@@ -47,6 +50,7 @@ sap.ui.define(
             sap.ui.getCore().setModel(oDisplayMsgAppModel, "DisplayMessageApp");
           }
           
+          this.getThisModel().setProperty("/DownloadBtnVisible", false);
           this.getThisModel().setProperty("/tileCustomUrl", this.getSaveTileCustomUrl);
         },
 
@@ -69,18 +73,92 @@ sap.ui.define(
           });
         },
         
+        /**
+	   * Event is triggered when message table selection is changed by user interaction
+       * @param {object} oEvent    Table Selction Change event
+	   * @public
+	   */
+        onMessageTableSelChange: function(oEvent) {
+        	this.handleDownloadBtnVisible(oEvent.getSource());
+        },
+        
       /**
-	  	 * Event is triggered before data loading of smart table
+	   * Event is triggered before data loading of smart table
        * @param {object} oEvent Table loading event
-	  	 * @public
-		   */
+	   * @public
+	   */
         onBeforeRebindTable: function(oEvent) {
-          var oUpdate = new SmartTableBindingUpdate(oEvent.getParameter('bindingParams'));
+          var oUpdate = new SmartTableBindingUpdate(oEvent.getParameter("bindingParams"));
           var aSorters = [];
           aSorters.push(new Sorter("Timestamp", true));
           aSorters.push(new Sorter("TransferDocumentNumber", true));
           oUpdate.addSorters(aSorters);
         },
+        
+        /**
+		 * Function is triggered once Message table is updated
+		 * @param {object} oEvent     Message Table Update Event
+		 * @public
+		 */
+        onMessageTableUpdateFinish: function(oEvent) {
+        	this.handleDownloadBtnVisible(oEvent.getSource());
+        },
+        
+        /**
+		 * Function handle visibility of download button on table selection change and table updation
+		 * @param {object} oEvent     Message Table Control
+		 * @public
+		 */
+        handleDownloadBtnVisible: function(oTableCntrl) {
+        	var aSelectedContexts = oTableCntrl.getSelectedContexts();
+        	var bDownloadBtnVisible = false;
+        	
+        	if(aSelectedContexts.length > 0) {
+        		bDownloadBtnVisible = true;
+        	}
+        	
+        	this.getThisModel().setProperty("/DownloadBtnVisible", bDownloadBtnVisible);
+        },
+        
+        /**
+		 * Function is triggered on click of Download button in List Page
+		 * @public
+		 */
+		onPressDownload: function() {
+			var aSelectedContexts = this.getView().byId("idMessageTable").getSelectedContexts();
+			var oSelectedObject = {};
+			var oJSZip = new JSZip();
+			var bRecordExceedLimit = false;
+			var sFolderName = "Market Message " + new Date().toLocaleDateString();
+			
+			for(var intI = 0; intI < aSelectedContexts.length && !bRecordExceedLimit; intI++) {
+				oSelectedObject = aSelectedContexts[intI].getObject();
+				oJSZip.file(oSelectedObject.ExternalUUID + ".txt", oSelectedObject.ExternalPayload);
+				
+				if(Object.keys(oJSZip.files).length > 50) {
+					bRecordExceedLimit = true;
+					delete oJSZip.files[oSelectedObject.ExternalUUID + ".txt"];
+				}
+			}
+			
+			if(bRecordExceedLimit) {
+				// Download confirmation popup
+				var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+				
+				MessageBox.confirm(oResourceBundle.getText("MESSAGE_LIST_DOWNLOAD_TEXT"), {
+					title: oResourceBundle.getText("MESSAGE_LIST_DOWNLOAD_TITLE"),
+					onClose: function(oAction) {
+						if (oAction === MessageBox.Action.OK) {
+							var oContent = oJSZip.generate({type:'blob'});
+							File.save(oContent, sFolderName,"zip","application/zip");
+						}
+					}.bind(this)
+				});
+			} else {
+				var oContent = oJSZip.generate({type:'blob'});
+				File.save(oContent, sFolderName,"zip","application/zip");
+			}
+		},
 
         /**
          * Event is triggered when FilterBar is initialized. 
