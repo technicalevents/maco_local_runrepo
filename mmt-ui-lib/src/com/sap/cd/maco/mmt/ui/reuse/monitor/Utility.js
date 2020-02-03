5f9ffe753ab79f8ed7e5e38afe4532edb24c66fa
Vaibhav Agarwal <agarwal.vaibhav046@gmail.com>
2020-02-03 13:19:07 +0000

    Update Service for Overview pages

--- mmt-ui-lib/src/com/sap/cd/maco/mmt/ui/reuse/monitor/Utility.js
+++ mmt-ui-lib/src/com/sap/cd/maco/mmt/ui/reuse/monitor/Utility.js
@@ -1,9 +1,10 @@
 sap.ui.define([
 	"com/sap/cd/maco/mmt/ui/reuse/monitor/Constants",
-	"sap/ui/model/odata/ODataUtils"
-], function(Constants, ODataUtils) {
+	"sap/ui/model/odata/ODataUtils",
+	"sap/ui/model/FilterOperator"
+], function (Constants, ODataUtils, FilterOperator) {
 	"use strict";
-	
+
 	return {
 		/**
 		 * Utility Function is used to convert record id into guid format
@@ -11,7 +12,7 @@ sap.ui.define([
 		 * @param {String} sId id to be converted into GUID format
 		 * @returns {String} id into GUID format
 		 */
-		convertToGuidFormat: function(sId) {
+		convertToGuidFormat: function (sId) {
 			return sId.replace(/(.{8})(.{4})(.{4})(.{4})(.{12})/, "$1-$2-$3-$4-$5").toLowerCase();
 		},
 
@@ -21,26 +22,26 @@ sap.ui.define([
 		 * @param   {String}   sId     id to be checked
 		 * @returns {String}           whether id is in guid format or not
 		 */
-		isGuid: function(sId) {
+		isGuid: function (sId) {
 			var sRegex = /[a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12}/i;
 			var oMatch = sRegex.exec(sId);
 			return oMatch != null;
 		},
 
 		/**
-		* Utility function to Get source array element with a given attribute (property) value
-		* @param {Array} aSourceArray - Search source
-		* @param {string} sAttribute - Attribute name
-		* @param {*} vValue - Attribute value to find
-		* @returns {object} Element or empty object, if search failed
-		* @public
-		*/
-		getObjectWithAttr: function(aSourceArray, sAttribute, vValue) {
+		 * Utility function to Get source array element with a given attribute (property) value
+		 * @param {Array} aSourceArray - Search source
+		 * @param {string} sAttribute - Attribute name
+		 * @param {*} vValue - Attribute value to find
+		 * @returns {object} Element or empty object, if search failed
+		 * @public
+		 */
+		getObjectWithAttr: function (aSourceArray, sAttribute, vValue) {
 			if (!Array.isArray(aSourceArray)) {
 				return {};
 			}
-			
-			return aSourceArray.find(function(oSourceElement) {
+
+			return aSourceArray.find(function (oSourceElement) {
 				return oSourceElement[sAttribute] === vValue;
 			}) || {};
 		},
@@ -50,13 +51,59 @@ sap.ui.define([
 		 * @public
 		 * @returns {String} guid
 		 */
-		generateGuid: function() {
+		generateGuid: function () {
 			function r() {
 				return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(
 					1);
 			}
 			return r() + r() + "-" + r() + "-" + r() + "-" + r() + "-" + r() + r() +
 				r();
+		},
+
+		/**
+		 * Function to modify Filter Conditions 
+		 * @public
+		 * @param {Object} oFilterData - Filter Data Object
+		 * @param {string} sOperator - FilterOperator
+		 * @param {Array} aProperties - property array that needs to be updated
+		 * @returns {String} Updated Filter Object
+		 */
+		modifyFilterDataWithOperator: function (oFilterData, sOperator, aProperties) {
+			var bIsFilterDataChanged = false;
+
+			aProperties.forEach(function (sProperty) {
+				var aFilterItem = [];
+				if (jQuery.isArray(oFilterData[sProperty].items) && oFilterData[sProperty].items.length > 0) {
+					bIsFilterDataChanged = true;
+					aFilterItem = oFilterData[sProperty].items;
+					for (var intI = 0; intI < aFilterItem.length; intI++) {
+
+						oFilterData[sProperty].ranges.push({
+							exclude: false,
+							keyField: sProperty,
+							value1: aFilterItem[intI].key,
+							value2: null,
+							operation: sOperator,
+							tokenText: "*" + aFilterItem[intI].key + "*"
+						});
+					}
+					oFilterData[sProperty].items = [];
+				} else {
+					aFilterItem = oFilterData[sProperty].ranges;
+					for (var intI = 0; intI < aFilterItem.length && aFilterItem[intI].operation != sOperator; intI++) {
+						oFilterData[sProperty].ranges[intI].operation = sOperator;
+						oFilterData[sProperty].ranges[intI].tokenText = "*" + aFilterItem[intI].tokenText.slice(1) + "*";
+						bIsFilterDataChanged = true;
+					}
+				}
+
+			});
+
+			if (!bIsFilterDataChanged) {
+				oFilterData = {}
+			}
+
+			return oFilterData;
 		}
 	};
 
--- overviewprocess/webapp/ext/ExtController.controller.js
+++ overviewprocess/webapp/ext/ExtController.controller.js
@@ -1,21 +1,43 @@
-sap.ui.define([],
-function() {
-  'use strict';
+sap.ui.define([
+		"sap/ui/model/FilterOperator",
+		"com/sap/cd/maco/mmt/ui/reuse/monitor/Utility"
+	],
+	function (FilterOperator, Utility) {
+		'use strict';
 
-  sap.ui.controller(
-    "com.sap.cd.maco.monitor.ui.app.overviewprocesses.ext.ExtController",
-    {
+		sap.ui.controller("com.sap.cd.maco.monitor.ui.app.overviewprocesses.ext.ExtController", {
 
-    /******************************************************************* */
-    /* LIFECYCLE METHODS */
-    /******************************************************************* */
-      
-    /**
-     * LifeCycle Method
-     */
-    onAfterRendering: function() {
-      this.getView().byId("mainView--ovpMain").addStyleClass("comSapCdMacoMmtUiMonitorMsgGraphTitle");
-      this.getView().byId("sapOvpShareButton").setVisible(false);;
-    }
-  });
-});
+			/******************************************************************* */
+			/* LIFECYCLE METHODS */
+			/******************************************************************* */
+
+			/**
+			 * LifeCycle Method
+			 */
+			onAfterRendering: function () {
+				this.getView().byId("mainView--ovpMain").addStyleClass("comSapCdMacoMmtUiMonitorMsgGraphTitle");
+				this.getView().byId("sapOvpShareButton").setVisible(false);
+			},
+			
+			/**
+			 * Event Handler for Filter Change
+			 * @public
+			 * @param {sap.ui.base.Event} oEvent Filter Change Event
+			 */
+			onGlobalFilterChange: function (oEvent) {
+				var oFilterData = jQuery.extend(true, {}, oEvent.getSource().getFilterData());
+				var oUpdatedFilterData = {};
+				
+				this.filterChanged = true;
+				
+				if(!jQuery.isEmptyObject(oFilterData)){
+					oUpdatedFilterData = Utility.modifyFilterDataWithOperator(oFilterData, FilterOperator.Contains, ["MarketPartner"]);
+				}
+				
+				if(!jQuery.isEmptyObject(oUpdatedFilterData)){
+					oEvent.getSource().setFilterData(oFilterData, true);
+				}
+
+			}
+		});
+	});
